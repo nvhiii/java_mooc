56aacac4fa72ab2b1e0673f8847f827d22cd943e
nvhiii <nahikhan16@gmail.com>
2024-04-22 20:48:34 -0400

    princeton 1.1 review done

---
+++ part1/HelloWorldMultilingual.java
@@ -0,0 +1,18 @@
+// Write a program HelloWorldMultilingual.java that prints 
+// "Hello World!" in as many human languages as you can.
+
+public class HelloWorldMultilingual {
+
+    public static void main(String[] args) {
+
+        System.out.println("Shagatam Prithivi!"); // Bengali
+        System.out.println("你好世界"); // Chinese
+        System.out.println("Hello World!"); // English
+        System.out.println("ہیلو دنیا والو"); // Urdu
+        System.out.println("반갑다 세상아"); // Korean
+        System.out.println("¡Hola mundo!"); // Spanish
+
+
+    }
+
+}
---
+++ part1/Initials.java
@@ -0,0 +1,17 @@
+// Write a program Initials.java that prints your 
+// initials using nine rows of asterisks like the one below
+
+public class Initials {
+
+    public static void main(String[] args) {
+
+        System.out.println("**     **  **     **");
+        System.out.println("***    **  **     * ");
+        System.out.println("** *   **  **    *  ");
+        System.out.println("**  *  **  ** ***   ");
+        System.out.println("**   * **  **    *  ");
+        System.out.println("**    ***  **      *");
+
+    }
+
+}
