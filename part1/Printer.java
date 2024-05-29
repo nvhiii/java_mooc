45898cde984788580e15f271f59073a58f9e0f2b
nvhiii <nahikhan16@gmail.com>
2024-05-28 20:57:31 -0400

    p3s3 done, onto p3s4 using strings

---
+++ part1/Printer.java
@@ -0,0 +1,27 @@
+// Complete the method public static void printArrayInStars(int[] array) 
+// in the class named 'Printer' to make it print a row of stars for 
+// each number in the array. The amount of stars on each row is 
+// defined by the corresponding number in the array.
+
+public class Printer {
+
+    public static void main(String[] args) {
+        // You can test the method here
+        int[] array = {5, 1, 3, 4, 2};
+        printArrayInStars(array);
+    }
+
+    public static void printArrayInStars(int[] array) {
+        
+        for (int i = 0; i < array.length; i++) {
+
+            for (int j = 0; j < array[i]; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+    }
+
+}
