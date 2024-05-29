7889bd7bba5a82ec5a016e676c9e4dea4390ea63
nvhiii <nahikhan16@gmail.com>
2024-05-29 17:53:38 -0400

    room.java done

---
+++ part1/Room.java
@@ -0,0 +1,13 @@
+public class Room {
+
+    private String code;
+    private int numberOfSeats;
+
+    public Room (String str, int num) {
+
+        this.code = str;
+        this.numberOfSeats = num;
+
+    }
+    
+}
