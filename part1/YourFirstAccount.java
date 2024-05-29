f0f5d3a1aed35f1c83d1337f078f9700c60f5a2c
nvhiii <nahikhan16@gmail.com>
2024-05-29 15:57:59 -0400

    your first bank transfer java done

---
+++ part1/YourFirstAccount.java
@@ -0,0 +1,11 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+        Account nahisAccount = new Account("nahi", 100.0);
+        nahisAccount.deposit(20.0);
+        System.out.println(nahisAccount);
+    }
+}
