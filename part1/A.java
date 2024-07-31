fd86c5ffffdaa3be7ddba0e7eda7803d8c954f59
nvhiii <nahikhan16@gmail.com>
2024-07-31 14:55:18 -0400

    basic inheritance

---
+++ part1/A.java
@@ -0,0 +1,19 @@
+// p1: Creating classes
+// Create the following three classes:
+
+// Class A. Class should have no object variables nor should you specify a constructor for it. It only has the method public void a(), which prints a string "A".
+// Class B. Class should have no object variables nor should you specify a constructor for it. It only has the method public void b(), which prints a string "B".
+// Class C. Class should have no object variables nor should you specify a constructor for it. It only has the method public void c(), which prints a string "C".
+
+// p2: Class inheritance
+// Modify the classes so that class B inherits class A, and class C inherits class B. In other words, class A will be a superclass for class B, and class B will be a superclass for class C.
+
+
+
+public class A {
+
+    public void a() {
+        System.out.println("A");
+    }
+    
+}
